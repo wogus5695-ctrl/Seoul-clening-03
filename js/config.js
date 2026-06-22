d9cfbd8e08b22a38d9aa7168c4d3c89075c2e1dc
wogus5695-ctrl <wogus5695@gmail.com>
2026-06-22 15:52:50 +0900

    fix: Remove test phone numbers

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // 사이트 전역 상수 데이터 관리
 
 const SITE_CONFIG = {
-    CONTACT_PHONE: "1588-0000",
-    CONTACT_SMS: "1588-0000",
+    CONTACT_PHONE: "",
+    CONTACT_SMS: "",
     BRAND_NAME: "클린폼(CLEAN FORME)"
 };
